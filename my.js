7804d66dc128900029d4dc4cd4ca85b69dfd7fe9
samod singh <samod0098@gmail.com>
2021-02-27 20:22:08 +0530

    if statement test

---
+++ my.js
@@ -0,0 +1,10 @@
+// if statement test
+var a = 10;
+if (a == 10);
+document.write(a + "<br>")
+
+// if statement logical operater
+var b = prompt("Enter your roll no")
+
+if (( b == 20))
+document.write("Name- samod kumar singh")
